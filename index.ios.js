f19189f6144163dc660aaa35e73bb93a8ae5a99f
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-06 15:47:05 -0700

    set index

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class insulince extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Insulince!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
