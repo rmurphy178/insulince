7a50c0215c7b997b608cf8d02e189f7e2cee3704
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-07 17:03:43 -0700

    Restart app with create react active app

---
+++ app/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import configureStore from '../store/store';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.store = configureStore();
+    window.store = this.store;
+  }
+
+  render () {
+    window.store = this.store;
+    return (
+      <Provider store={this.store}>
+        <View style={styles.container}>
+          <Text>Insulince</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
