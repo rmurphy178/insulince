f3b5841588700405a9b5c520eb28872482e2a5af
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-08-07 19:11:24 -0700

    adding auth folder

---
+++ app/components/screens/auth/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class SignUp extends Component {
+
+}
+
+export default SignUp
+
+const styles = StyleSheet.create({
+
+})
---
+++ app/components/screens/styles/styles.js
@@ -0,0 +1,12 @@
+const baseStyles = {
+  // Screen flexes 'navs' and main content (container)
+  screen: {
+    flex: 1,
+    marginTop: 25
+  },
+
+
+
+}
+
+export default baseStyles
