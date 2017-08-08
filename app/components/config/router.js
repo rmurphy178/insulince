bd96913f552d924978ae47ee9d3f7401f041b8f3
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-08 12:39:44 -0700

    auth routing for signup and login containers

--- app/components/config/router.js
+++ app/components/config/router.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 
-// TODO:
+// Tab Screens
 
-// Decide which routes should use StackNavigator(views represented as stack of cards)
+// Stack Screens
+import LoginContainer from '../screens/auth/login_container';
+import SignUpContainer from '../screens/auth/signup_container';
 
-// and which should use TabNavigator (tabs within a screen).
 
-// DrawerNavigator(slides out across part of screen from left), Modal(from bottom),
-
-// and custom navigation views also possible but only StackRouter and TabRouter
-
-// included as standard routers. Top level navigators
-
-// accept these props: onNavigationStateChange(prevState, newState, action) 
+export const AuthNav = StackNavigator({
+  Login: {
+    screen: LoginContainer
+  },
+  SignUp: {
+    screen: SignUpContainer
+    }
+  },
+  { headerMode: 'none'}
+);
