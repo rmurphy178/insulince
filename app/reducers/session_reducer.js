87a785693c352c6e17e31a6e3454318ccb7d2b2b
Alice Chen <alicechen118@gmail.com>
2017-08-07 19:15:25 -0700

    Fix session reducer object freeze statement

--- app/reducers/session_reducer.js
+++ app/reducers/session_reducer.js
@@ -2,7 +2,9 @@ import { RECEIVE_CURRENT_USER, CLEAR_STORE } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 const SessionReducer = (state = null, action) => {
-  Object.freeze(state);
+  if (state) {
+    Object.freeze(state);
+  }
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
       return action.currentUser;
