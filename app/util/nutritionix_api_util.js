2b7160e851d998fcb0b7c360fc2b40b2493b12e9
Alice Chen <alicechen118@gmail.com>
2017-08-09 18:47:14 -0700

    Start building axios requests to nutritionix api

--- app/util/nutritionix_api_util.js
+++ app/util/nutritionix_api_util.js
@@ -1,1 +1,25 @@
 import axios from 'axios';
+import NUTRITIONIX_APP_ID from '../../constants';
+import NUTRITIONIX_APP_KEY from '../../constants';
+
+const fields = ["item_name, brand_name, item_id, brand_id, upc, nf_calories, nf_calories_from_fat, nf_total_fat, nf_total_fat, nf_saturated_fat, nf_monounsaturated_fat, nf_polyunsaturated_fat, nf_trans_fatty_acid, nf_cholesterol, nf_sodium, nf_total_carbohydrate, nf_dietary_fiber, nf_sugars, nf_protein, nf_serving_size_qty, nf_serving_size_unit"];
+
+export const searchNutritionix = query => {
+  axios({
+    method: 'POST',
+    url: 'https://api.nutritionix.com/v1_1/search',
+    data: {
+      "appId": NUTRITIONIX_APP_ID,
+      "appKey": NUTRITIONIX_APP_KEY,
+      "fields": fields,
+      "sort": {
+        "field": "_score",
+        "order": "desc"
+      },
+      "query": query
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
