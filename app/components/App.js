8467e962782ff4462a8060827517be1b1a72b55e
Alice Chen <alicechen118@gmail.com>
2017-08-11 12:33:08 -0700

    Load expo fonts before load app

--- app/components/App.js
+++ app/components/App.js
@@ -3,25 +3,47 @@ import configureStore from '../store/store';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  StatusBar
 } from 'react-native';
 import { Provider } from 'react-redux';
 import { AuthNav } from './config/router';
+import Expo, { AppLoading } from "expo";
+import { Root } from "native-base";
 
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
     this.store = configureStore();
+    this.state = {
+      ready: false
+    }
+  }
 
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ ready: true });
   }
 
   render () {
-    return (
-      <Provider store={this.store}>
-        <AuthNav />
-      </Provider>
-    );
+    if (!this.state.ready) {
+      return <AppLoading />;
+    } else {
+      return (
+        <Provider store={this.store}>
+          <Root>
+            <StatusBar hidden={true} />
+            <AuthNav />
+          </Root>
+        </Provider>
+      );
+    }
   }
 }
 
