4f4118acad2b0bfb551cdf89e7a8091425a18678
Alice Chen <alicechen118@gmail.com>
2017-08-11 12:32:28 -0700

    Add searchbar to food search component

--- app/components/screens/food_search/food_search_container.js
+++ app/components/screens/food_search/food_search_container.js
@@ -1,19 +1,38 @@
-import { connect } from 'react-redux';
-import FoodSearch from 'food_search';
+import React, { Component } from 'react';
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+import getTheme from '../../../../native-base-theme/components';
+import material from '../../../../native-base-theme/variables/material';
 
-const mapStateToProps = state => {
-  return {
+export default class FoodSearch extends Component {
 
-  };
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+    this.updateQuery = this.updateQuery.bind(this);
+  }
 
-const mapDispatchToProps = dispatch => {
-  return {
+  updateQuery(event) {
+    this.setState({
+      query: event.target.value
+    });
+  }
 
-  };
-};
+  render() {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search for a food" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+      </Container>
+    );
+  }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FoodSearch);
+}
