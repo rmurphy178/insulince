618ec7ad11464710bcd0c92514a94a51db123fa8
Alice Chen <alicechen118@gmail.com>
2017-08-10 18:54:50 -0700

    Update session actions and api util

--- app/actions/session_actions.js
+++ app/actions/session_actions.js
@@ -21,18 +21,22 @@ export const clearStore = () => {
 export const signUp = user => dispatch => {
   return(
     sessionAPIUtil.signUp(user)
-      .then(userInfo => login(userInfo),
-        errors => dispatch(receiveErrors(errors)))
+      .then(response => {
+        ASYNC.setItem("auth_token", response.data.auth_token);
+        dispatch(receiveCurrentUser(response.data));
+        dispatch(clearErrors());
+      }, errors => dispatch(receiveErrors(errors.response.data)))
   );
 };
 
 export const login = user => dispatch => {
   return(
     sessionAPIUtil.login(user)
-      .then(currentUser => {
-        dispatch(receiveCurrentUser(currentUser));
+      .then(response => {
+        ASYNC.setItem("auth_token", response.data.auth_token);
+        dispatch(receiveCurrentUser(response.data));
         dispatch(clearErrors());
-      }, errors => dispatch(receiveErrors(errors)))
+      }, errors => dispatch(receiveErrors(["Invalid credentials"])))
   );
 };
 
--- app/util/session_api_util.js
+++ app/util/session_api_util.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 import ASYNC from '../util/async_util';
+import { receiveLoginErrors } from '../actions/errors_actions';
 
 export const signUp = user => {
+  console.log(user);
   const url = "https://insulince-api.herokuapp.com/api/users";
   return axios.post(url, { user })
-    .then(response => console.log(response));
+    .then(response => axios.post("https://insulince-api.herokuapp.com/api/user_token", {
+      auth: {
+        user_credential: user.username,
+        password: user.password
+      }
+    }));
 };
 
 export const login = user => {
-  console.log(user);
   const url = "https://insulince-api.herokuapp.com/api/user_token";
-  return axios.post(url, { auth: user })
-    .then(ASYNC.setItem("auth_token", user.auth_token));
+  return axios.post(url, {
+    auth: {
+      user_credential: user.userCredential,
+      password: user.password
+    }
+  });
 };
