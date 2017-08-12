9bf6cf12ede9bd8d5d853a815981460dfe6493f0
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-12 16:30:28 -0700

    base styles

---
+++ app/components/screens/styles/styles.js
@@ -0,0 +1,35 @@
+const baseStyles = {
+  inputContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#F2C500'
+
+  },
+  input: {
+    textAlign: 'center',
+    height: 50,
+    margin: 2,
+    marginTop: 0,
+    paddingBottom: 1,
+    color: 'white'
+
+  },
+  buttonContainer: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#F2C500',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 45,
+    borderRadius: 15,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#A4FFF8'
+  }
+
+
+
+};
+
+export default baseStyles;
