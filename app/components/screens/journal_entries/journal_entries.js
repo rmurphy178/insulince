069bfeb3a0e4b4b72ceda0d0bf0b476af338eef3
Alice Chen <alicechen118@gmail.com>
2017-08-11 22:15:22 -0700

    Insert placeholder list items for food journal component

--- app/components/screens/journal_entries/journal_entries.js
+++ app/components/screens/journal_entries/journal_entries.js
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native';
 import React, { Component } from 'react';
 import { fetchAllJournalEntries } from '../../../util/journal_entries_api_util';
+import { Container, Content, List, ListItem, Text, Separator } from 'native-base';
 
 
 class JournalEntries extends React.Component {
@@ -8,12 +8,57 @@ class JournalEntries extends React.Component {
     this.props.fetchAllJournalEntries();
   }
 
+  constructor(props) {
+    super(props);
+  }
 
   render() {
+    var items = ['Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can'];
     return (
-      <Text>
-        JournalEntries
-      </Text>
+      <Container>
+        <Content>
+          <Separator bordered>
+            <Text>BREAKFAST</Text>
+          </Separator>
+          <List dataArray={items}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            }>
+          </List>
+          <Separator bordered>
+            <Text>LUNCH</Text>
+          </Separator>
+          <List dataArray={items}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            }>
+          </List>
+          <Separator bordered>
+            <Text>DINNER</Text>
+          </Separator>
+          <List dataArray={items}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            }>
+          </List>
+          <Separator bordered>
+            <Text>SNACKS</Text>
+          </Separator>
+          <List dataArray={items}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            }>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
