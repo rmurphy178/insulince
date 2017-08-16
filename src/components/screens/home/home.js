23d07b85c3207e08e73d27db1072af155e653473
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-15 23:26:54 -0700

    breakdown carbs by meal item and reduce to single chart field

--- src/components/screens/home/home.js
+++ src/components/screens/home/home.js
@@ -28,17 +28,31 @@ export default class Home extends React.Component {
       dinnerItems_calories: "",
       snackItems_calories: "",
       calories: "",
+
       breakfastItems_sugar: "",
       lunchItems_sugar: "",
       dinnerItems_sugar: "",
       snackItems_sugar: "",
+      sugar: "",
+
+
       breakfastItems_fat: "",
       lunchItems_fat: "",
       dinnerItems_fat: "",
       snackItems_fat: "",
-      sugar: "",
       fat: "",
+
+
+      breakfastItems_carbs: "",
+      lunchItems_carbs: "",
+      dinnerItems_carbs: "",
+      snackItems_carbs: "",
       carbs: "",
+
+      breakfastItems_protein: "",
+      lunchItems_protein: "",
+      dinnerItems_protein: "",
+      snackItems_protein: "",
       protein: ""
     };
   }
@@ -52,64 +66,91 @@ export default class Home extends React.Component {
         this.breakfastItems_calories = [];
         this.breakfastItems_sugar = [];
         this.breakfastItems_fat = [];
+        this.breakfastItems_carbs = [];
+
         this.lunchItems_calories = [];
         this.lunchItems_sugar = [];
         this.lunchItems_fat = [];
+        this.lunchItems_carbs = [];
+
         this.dinnerItems_calories = [];
         this.dinnerItems_sugar = [];
         this.dinnerItems_fat = [];
+        this.dinnerItems_carbs = [];
+
         this.snackItems_calories = [];
         this.snackItems_sugar = [];
         this.snackItems_fat = [];
+        this.snackItems_carbs = [];
+
         journalEntries.byId[this.currentEntryId].breakfast
           .forEach(breakfastItem => {
             this.breakfastItems_calories.push(breakfastItem['nf_calories']);
             this.breakfastItems_sugar.push(breakfastItem['nf_sugars']);
             this.breakfastItems_fat.push(breakfastItem['nf_total_fat']);
+            this.breakfastItems_carbs.push(breakfastItem['nf_total_carbohydrate']);
           });
         journalEntries.byId[this.currentEntryId].lunch
           .forEach(lunchItem => {
             this.lunchItems_calories.push(lunchItem['nf_calories']);
             this.lunchItems_sugar.push(lunchItem['nf_sugars']);
             this.lunchItems_fat.push(lunchItem['nf_total_fat']);
+            this.lunchItems_carbs.push(lunchItem['nf_total_carbohydrate']);
           });
         journalEntries.byId[this.currentEntryId].dinner
           .forEach(dinnerItem => {
             this.dinnerItems_calories.push(dinnerItem['nf_calories']);
             this.dinnerItems_sugar.push(dinnerItem['nf_sugars']);
             this.dinnerItems_fat.push(dinnerItem['nf_total_fat']);
+            this.dinnerItems_carbs.push(dinnerItem['nf_total_carbohydrate']);
         });
         journalEntries.byId[this.currentEntryId].snacks
           .forEach(snackItem => {
             this.snackItems_calories.push(snackItem['nf_calories']);
             this.snackItems_sugar.push(snackItem['nf_sugars']);
             this.snackItems_fat.push(snackItem['nf_total_fat']);
+            this.snackItems_carbs.push(snackItem['nf_total_carbohydrate']);
           });
         this.setState({
           breakfastItems_calories: this.breakfastItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))),
           breakfastItems_sugar: this.breakfastItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))),
           breakfastItems_fat: this.breakfastItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))),
+          breakfastItems_carbs: this.breakfastItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))),
+
           lunchItems_calories: this.lunchItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))),
           lunchItems_sugar: this.lunchItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))),
           lunchItems_fat: this.lunchItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))),
+          lunchItems_carbs: this.lunchItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))),
+
           dinnerItems_calories: this.dinnerItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))),
           dinnerItems_sugar: this.dinnerItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))),
           dinnerItems_fat: this.dinnerItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))),
+          dinnerItems_carbs: this.dinnerItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))),
+
           snackItems_calories: this.snackItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))),
           snackItems_sugar: this.snackItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))),
           snackItems_fat: this.snackItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))),
+          snackItems_carbs: this.snackItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))),
+
           calories: (this.breakfastItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))) +
                       this.lunchItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))) +
                       this.dinnerItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories)))) +
                       this.snackItems_calories.reduce((total, calories) => (Math.floor(Number(total) + Number(calories))))),
+
           sugar: (this.breakfastItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))) +
                       this.lunchItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))) +
                       this.dinnerItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar)))) +
                       this.snackItems_sugar.reduce((total, sugar) => (Math.floor(Number(total) + Number(sugar))))),
+
           fat: (this.breakfastItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))) +
                       this.lunchItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))) +
                       this.dinnerItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))) +
-                      this.snackItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat)))))
+                      this.snackItems_fat.reduce((total, fat) => (Math.floor(Number(total) + Number(fat))))),
+
+          carbs: (this.breakfastItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))) +
+                      this.lunchItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))) +
+                      this.dinnerItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))) +
+                      this.snackItems_carbs.reduce((total, carbs) => (Math.floor(Number(total) + Number(carbs)))))
         });
       });
   }
